b0432b63cc9c6201535d02cfd68f03dd5f7eab1e
Kabachok <94787801+kerrigan-nataly@users.noreply.github.com>
2022-05-01 18:06:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -303,5 +303,36 @@ def choice(planet_name):
             """
 
 
+@app.route('/results/<nickname>/<int:level>/<float:rating>')
+def results(nickname, level, rating):
+    return f"""<!doctype html>
+                <html lang="ru">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                    <link rel="stylesheet" 
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                    crossorigin="anonymous">
+                    <title>Результаты</title>
+                  </head>
+                  <body class="container">
+                    <h1>Результаты отбора</h1>
+                    <h2>Претендерта на участие в миссии {nickname}:<h2>
+
+                    <div class="alert alert-success" role="alert">
+                      <h3>Поздравляем! Ваш рейтинг после {level} этапа отбора</h3>
+                    </div>
+                    <div class="alert" role="alert">
+                      <h3>составляет {rating}!</h3> 
+                    </div>
+                    <div class="alert alert-warning" role="alert">
+                      <h2>Желаем удачи!</h2>
+                    </div>
+                  </body>
+                </html>
+            """
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
